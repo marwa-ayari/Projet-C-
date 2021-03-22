5a98e4415d98f19c68d23dd23ea24d31f1863e03
Eyakhechine <eya_khechine@hotmail.com>
2021-03-22 17:01:47 +0100

    gestion employés congés

--- gestionEC/mainwindow.cpp
+++ gestionEC/mainwindow.cpp
@@ -9,6 +9,8 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
+
     ui->setupUi(this);
     son = new QSound("../music.wav");
     son->play();
