2de38e297de373b46f9ca09b9e7cfef376a4719c
marwa-ayari <maroua.ayari@esprit.tn>
2021-03-23 13:35:09 +0100

    Controle saisie

--- Gestion_catégorie_réclamation/gestion_categories_reclamations.cpp
+++ Gestion_catégorie_réclamation/gestion_categories_reclamations.cpp
@@ -1,6 +1,6 @@
 #include "gestion_categories_reclamations.h"
 #include "ui_gestion_categories_reclamations.h"
-
+#include <QDateTime>
 #include "categories.h"
 #include"reclamations.h"
 #include <QSqlQuery>
@@ -25,19 +25,28 @@ Gestion_categories_reclamations::~Gestion_categories_reclamations()
 void Gestion_categories_reclamations::on_pushButton_categorie_4_clicked()
 {
     QString nom= ui->lineEdit_categorie_12->text();
-    QString date_fab= ui->dateEdit_categorie_7->text();
+    QString date_fab= ui->dateEdit_categorie_7->date().toString("yyyy/MM/dd");
     float prix=ui->lineEdit_categorie_13->text().toFloat();
-    Categories catg(nom,date_fab,prix);
-    if(catg.ajouter()) {
-        QMessageBox::information(nullptr, QObject::tr("BRAVO!!!"),
-                    QObject::tr("Ajout effectuer avec succés.\n" ), QMessageBox::Cancel);
-        ui->lineEdit_categorie_12->setText("");
-        ui->lineEdit_categorie_13->setText("");
+    QString system_date = QDateTime::currentDateTime().toString("yyyy/MM/dd");
 
-}else{
-        QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
-                    QObject::tr("Cette catégorie existe déja.\n"), QMessageBox::Cancel);
-    }
+    if((prix>0)&&(system_date>=date_fab))
+    {
+        date_fab= ui->dateEdit_categorie_7->text();
+        Categories catg(nom,date_fab,prix);
+        if(catg.ajouter()) {
+            QMessageBox::information(nullptr, QObject::tr("BRAVO!!!"),
+                        QObject::tr("Ajout effectuer avec succés.\n" ), QMessageBox::Cancel);
+            ui->lineEdit_categorie_12->setText("");
+            ui->lineEdit_categorie_13->setText("");
+
+    }else{
+            QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
+                        QObject::tr("Cette catégorie existe déja.\n"), QMessageBox::Cancel);
+        }
+
+    }else QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
+                                   QObject::tr("le prix doit etre supérieure à 0 et la date ne doit pas dépasser la date d'aujourd'hui .\n"), QMessageBox::Cancel);
+
 
 }
 
@@ -100,9 +109,15 @@ void Gestion_categories_reclamations::on_comboBox_categorie_modifi_currentIndexC
 void Gestion_categories_reclamations::on_pushButton_categorie_7_clicked()
 {
     QString nom= ui->comboBox_categorie_modifi->currentText();
-    QString date_fab= ui->dateEdit_categorie_2->text();
+    QString date_fab= ui->dateEdit_categorie_2->date().toString("yyyy/MM/dd");
     float prix=ui->lineEdit_categorie_5->text().toFloat();
+    QString system_date = QDateTime::currentDateTime().toString("yyyy/MM/dd");
+    if((prix>0)&&(system_date>=date_fab))
+    {
+    date_fab= ui->dateEdit_categorie_2->text();
+
     Categories catg(nom,date_fab,prix);
+
     if(catg.modifier())
     {
               QMessageBox::information(nullptr, QObject::tr("Modifier une catégorie"),
@@ -114,6 +129,11 @@ void Gestion_categories_reclamations::on_pushButton_categorie_7_clicked()
         QMessageBox::critical(nullptr, QObject::tr("Modifier une catégorie"),
                           QObject::tr("Erreur !\n"), QMessageBox::Cancel);
     }
+
+    }else QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
+                                   QObject::tr("le prix doit etre supérieure à 0 et la date ne doit pas dépasser la date d'aujourd'hui .\n"), QMessageBox::Cancel);
+
+
 }
 
 void Gestion_categories_reclamations::on_bouton_ajout_reclamation_clicked()
@@ -121,7 +141,12 @@ void Gestion_categories_reclamations::on_bouton_ajout_reclamation_clicked()
     QString nom_categ= ui->comboBox_reclamation_1->currentText();
     QString id= ui->comboBox_reclamation_2->currentText();
     QString avis_c= ui->avis_reclamation->text();
-    QString date_rec= ui->date_reclamation->text();
+    QString date_rec= ui->date_reclamation->date().toString("yyyy/MM/dd");
+    QString system_date = QDateTime::currentDateTime().toString("yyyy/MM/dd");
+
+    if(system_date>=date_rec)
+    {
+        date_rec= ui->date_reclamation->text();
     reclamations rec(nom_categ,id,date_rec,avis_c);
     if(rec.ajouter()) {
         QMessageBox::information(nullptr, QObject::tr("BRAVO!!!"),
@@ -133,6 +158,8 @@ void Gestion_categories_reclamations::on_bouton_ajout_reclamation_clicked()
         QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
                     QObject::tr("Cette réclamation existe déja.\n"), QMessageBox::Cancel);
     }
+    }else QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
+                                   QObject::tr("la date ne doit pas dépasser la date d'aujourd'hui .\n"), QMessageBox::Cancel);
 
 }
 
@@ -163,11 +190,6 @@ void Gestion_categories_reclamations::on_pushButton_12_clicked()
     ui->tab_reclamation->setModel(tmpreclamation.tri());
 }
 
-
-
-
-
-
 void Gestion_categories_reclamations::on_comboBox_reclamation_3_currentIndexChanged(const QString &arg1)
 {
     QString nom=ui->comboBox_reclamation_3->currentText();
@@ -192,7 +214,13 @@ void Gestion_categories_reclamations::on_pushButton_2_clicked()
     QString nom= ui->comboBox_reclamation_3->currentText();
     QString id_client= ui->comboBox_reclamation_4->currentText();
     QString avis= ui->LineEdit_reclamation_3->text();
-    QString date_rec= ui->dateEdit_reclamation_2->text();
+    QString date_rec= ui->dateEdit_reclamation_2->date().toString("yyyy/MM/dd");
+    QString system_date = QDateTime::currentDateTime().toString("yyyy/MM/dd");
+
+    if(system_date>=date_rec)
+    {
+
+    date_rec= ui->dateEdit_reclamation_2->text();
     reclamations rec(nom,id_client,date_rec,avis);
     if(rec.modifier())
     {
@@ -205,9 +233,18 @@ else
         QMessageBox::critical(nullptr, QObject::tr("Erreur !"),
                           QObject::tr("Cette réclamation n'existe pas.\nVeuillez vérifier le nom de catégorie et le id client."), QMessageBox::Cancel);
     }
+    }else QMessageBox::information(nullptr, QObject::tr("ECHEC!!!"),
+                                   QObject::tr("la date ne doit pas dépasser la date d'aujourd'hui .\n"), QMessageBox::Cancel);
+
 }
 
 void Gestion_categories_reclamations::on_tabWidget_categorie_reclamation_2_currentChanged(int index)
+{
+    ui->comboBox_reclamation_1->setModel(tmpreclamation.affecter_Categorie());
+    ui->comboBox_reclamation_2->setModel(tmpreclamation.affecter_Client());
+}
+
+void Gestion_categories_reclamations::on_tabWidget_reclamation_3_currentChanged(int index)
 {
     ui->tab_reclamation->setEnabled(true);
     ui->tab_reclamation->setModel(tmpreclamation.afficher());
@@ -217,10 +254,4 @@ void Gestion_categories_reclamations::on_tabWidget_categorie_reclamation_2_curre
     ui->comboBox_reclamation_4->setModel(tmpreclamation.affecter_Client());
     ui->comboBox_reclamation_5->setModel(tmpreclamation.affecter_Categorie());
     ui->comboBox_reclamation_6->setModel(tmpreclamation.affecter_Client());
-}
-
-void Gestion_categories_reclamations::on_tabWidget_reclamation_3_currentChanged(int index)
-{
-    ui->tab_reclamation->setModel(tmpreclamation.afficher());
-
 }
