dbf88f44b8a94ab2db5b00a08d359f4e24bad7b0
Ben Amor Eya <eya.benamor@esprit.tn>
2021-03-24 01:28:48 +0100

    animation bouton connecter

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,14 +3,29 @@
 #include"clients_cadeaux.h"
 #include <QMediaPlayer>
 #include<QMessageBox>
+#include<QString>
+#include <QSound>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QMediaPlayer *music=new QMediaPlayer();
-       music->setMedia(QUrl("C:/images_projets/music.wav"));
-       music->play();
+
+       son = new QSound("C:/images_projets/music.wav");
+          son->play();
+          son->setLoops(20);
+          setFixedSize(800,600);
+          animation = new QPropertyAnimation(ui->pushButton_connecter,"geometry");
+             animation->setDuration(20000);
+             animation->setStartValue(ui->pushButton_connecter->geometry());
+             animation->setEndValue(QRectF(200,300,70,70));
+
+             QEasingCurve curve;
+             curve.setType(QEasingCurve::OutBounce);
+             animation->setEasingCurve(curve);
+
+             animation->start();
+
 
 
 }
@@ -21,19 +36,46 @@ MainWindow::~MainWindow()
 
 
 
-void MainWindow::on_pushButton_clicked()
-{  
-    QString nom = ui->lineEdit_nom_prop->text();
-        QString prenom = ui->lineEdit_prenom_prop->text();
-        QString mdp = ui->lineEdit_mdp_prop->text();
 
-          /*     if((nom ==  "Ben Amor")&&(prenom=="Eya")&&(mdp =="prop")) {*/
-    clients_cadeaux fenetre;
-    fenetre.exec();
-               }
-               /*else
-               {
-                   QMessageBox::warning(this,"Connection", "Vérifier svp ");
-               }
+void MainWindow::on_pushButton_connecter_clicked()
+{
+    QMediaPlayer *player = new QMediaPlayer;
+        player->setMedia(QUrl::fromLocalFile("C:/images_projets/button.mp3"));
+        player->setVolume(2000);
+        player->play();
+
+            QString id = ui->lineEdit_login->text();
+            QString mdp = ui->lineEdit_mdp->text();
+
+              /*     if((id ==  "Ben Amor")&&(mdp =="prop")) {*/
+        clients_cadeaux fenetre;
+        fenetre.exec();
+
+                   /*else
+                   {
+                       QMessageBox::warning(this,"Connection", "Vérifier svp ");
+                   }
+
+    */
+
 }
-*/
+void MainWindow::on_pushButton_off_clicked()
+{  QMediaPlayer *player = new QMediaPlayer;
+    player->setMedia(QUrl::fromLocalFile("C:/images_projets/button.mp3"));
+    player->setVolume(2000);
+    player->play();
+
+
+    son->stop();
+
+}
+
+void MainWindow::on_pushButton_on_clicked()
+{  QMediaPlayer *player = new QMediaPlayer;
+    player->setMedia(QUrl::fromLocalFile("C:/images_projets/button.mp3"));
+    player->setVolume(2000);
+    player->play();
+    son->play();
+
+
+}
