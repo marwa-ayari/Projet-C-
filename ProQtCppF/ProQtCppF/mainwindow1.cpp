10b36dc922695924cffffd419e8b183dd4051c14
eyabelguith <eyabelguith@esprit.tn>
2021-04-18 00:30:32 +0200

    uiop[

---
+++ ProQtCppF/ProQtCppF/mainwindow1.cpp
@@ -0,0 +1,81 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "commande.h"
+#include"livraison.h"
+
+#include <QtMultimedia/QSound>
+
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    son = new QSound("../imagesQT/music.wav");
+    son->play();
+    son->setLoops(50);
+    setFixedSize(990,600);  //fixe la taille de la fenêtre
+
+    animation = new QPropertyAnimation(ui->login,"geometry");
+    animation->setDuration(8000);
+    animation->setStartValue(ui->login->geometry());
+    animation->setEndValue(QRectF(200,300,90,70));
+
+
+    QEasingCurve curve;
+    curve.setType(QEasingCurve::OutBounce);
+    animation->setEasingCurve(curve);
+    animation->start();
+
+
+
+
+
+
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+
+void MainWindow::on_login_clicked()
+{
+    QString username=ui->username->text();
+    QString password=ui->password->text();
+    if((username=="eya")&&(password=="eya"))
+    {
+        gesempcong gestion;
+        gestion.setModal(true);
+        gestion.exec();
+
+    }else QMessageBox::critical(nullptr, QObject::tr("acces refuse"),
+                                QObject::tr("Vérifiez vos coordonnées."), QMessageBox::Cancel);
+}
+
+
+
+
+
+
+
+
+void MainWindow::on_sonon_clicked()
+{ son->play();
+
+}
+
+void MainWindow::on_sonoff_clicked()
+{son->stop();
+
+}
+
+
---
+++ ProQtCppF/ProQtCppF/mainwindow1.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtMultimedia/QSound>
+#include <QPropertyAnimation>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+
+
+
+
+private slots:
+
+    void on_sonon_clicked();
+
+    void on_sonoff_clicked();
+
+    void on_login_clicked();
+
+signals :
+    void sig();
+private:
+    Ui::MainWindow *ui;
+    QSound *son;
+    QPropertyAnimation *animation;
+
+
+
+
+};
+#endif // MAINWINDOW_H
